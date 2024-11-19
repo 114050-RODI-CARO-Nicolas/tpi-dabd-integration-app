b33efff6dbee44916471706c4c8c03f8b0a43afb
404931-SAGRIPANTI-VALENTINO <164201852+404931-SAGRIPANTI-VALENTINO@users.noreply.github.com>
2024-11-19 16:32:19 -0300

    Update my-notification.component.ts

--- src/app/notifications/modules/components/my-notification/my-notification.component.ts
+++ src/app/notifications/modules/components/my-notification/my-notification.component.ts
@@ -77,7 +77,7 @@ export class MyNotificationComponent implements OnInit {
           },
           error: (err) => {
             console.log("Error al cargar las notificacione")
-            this.toastService.sendError("No se encontró un contacto válido en el usuario con la sesión inicada.")
+            this.toastService.sendError("No se encontró un contacto válido en el usuario con la sesión iniciada.")
           }
         }
       );
