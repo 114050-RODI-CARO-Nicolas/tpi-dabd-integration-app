cefb8a5e0f2647b5d3ee47d1398b945eea1e0372
Val <404931@tecnicatura.frc.utn.edu.ar>
2024-11-19 20:26:25 -0300

    fix

--- src/app/notifications/models/notifications/notification.ts
+++ src/app/notifications/models/notifications/notification.ts
@@ -1,23 +1,33 @@
 export interface Notification {
-    id: number;
-    recipient: string;
-    contactId?: number;
-    subject: string;
-    templateId: number;
-    templateName: string;
-    statusSend: string;
-    dateSend: string | Date;
-    body: string;
-    isRead?: boolean;
-    dateNotification?: string | Date;
-}
-
-export interface NotificationModelChart {
   id: number;
   recipient: string;
+  contactId?: number;
   subject: string;
   templateId: number;
   templateName: string;
-  dateSend: string;
   statusSend: string;
+  dateSend: string | Date;
+  body: string;
+  isRead?: boolean;
+  dateNotification?: string | Date;
+}
+
+export interface NotificationModelChart {
+id: number;
+recipient: string;
+subject: string;
+templateId: number;
+templateName: string;
+dateSend: string;
+statusSend: string;
+}
+
+export interface NotificationKPIViewedModel {
+id: number;
+recipient: string;
+subject: string;
+templateId: number;
+templateName: string;
+dateSend: string;
+statusSend: 'SENT' | 'VISUALIZED';
 }
